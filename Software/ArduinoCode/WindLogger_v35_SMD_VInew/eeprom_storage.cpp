1477bc692a49f0d78e3e4773e2c3af9f19731daf
James Fowkes <jamesfowkes@gmail.com>
2015-08-05 19:37:16 +0100

    Moved EEPROM stoage locations to enum

--- Software/ArduinoCode/WindLogger_v35_SMD_VInew/eeprom_storage.cpp
+++ Software/ArduinoCode/WindLogger_v35_SMD_VInew/eeprom_storage.cpp
@@ -12,6 +12,25 @@
 #include <EEPROM.h>
 
 /*
+ * Defines and Typedefs
+ */
+
+/*
+ * An enumeration of the EEPROM storage locations.
+ * Byte-wise indexing, so take datatype length into account
+ */
+
+enum eeprom_locations_enum
+{
+	LOC_DEVICE_ID = 0,
+	LOC_SAMPLE_TIME = 2,
+	LOC_CURRENT_OFFSET = 4,
+	LOC_R1 = 6,
+	LOC_R2 = 8,
+	LOC_CURRENT_GAIN = 10
+};
+
+/*
  * Public Functions
  */
 
@@ -19,8 +38,8 @@ void EEPROM_GetDeviceID(char * buffer)
 {
 	if (buffer)
 	{
-		buffer[0] = (char)EEPROM.read(0);
-		buffer[1] = (char)EEPROM.read(1);
+		buffer[0] = (char)EEPROM.read(LOC_DEVICE_ID);
+		buffer[1] = (char)EEPROM.read(LOC_DEVICE_ID+1);
 	}
 }
 
@@ -28,62 +47,62 @@ void EEPROM_SetDeviceID(char * buffer)
 {
 	if (buffer)
 	{
-		EEPROM.write(0,buffer[0]);
-		EEPROM.write(1,buffer[1]);
+		EEPROM.write(LOC_DEVICE_ID,buffer[0]);
+		EEPROM.write(LOC_DEVICE_ID+1,buffer[1]);
 	}
 }
 
 uint16_t EEPROM_GetSampleTime(void)
 {
-	return (EEPROM.read(2) << 8) + EEPROM.read(3);	
+	return (EEPROM.read(LOC_SAMPLE_TIME+1) << 8) + EEPROM.read(LOC_SAMPLE_TIME);	
 }
 
 void EEPROM_SetSampleTime(uint16_t sampleTime)
 {
-	EEPROM.write(2, sampleTime >> 8);
-    EEPROM.write(3, sampleTime & 0xff);
+	EEPROM.write(LOC_SAMPLE_TIME, sampleTime >> 8);
+    EEPROM.write(LOC_SAMPLE_TIME+1, sampleTime & 0xff);
 }
 
 uint16_t EEPROM_GetCurrentOffset(void)
 {
-	return (EEPROM.read(4) << 8) + EEPROM.read(5);
+	return (EEPROM.read(LOC_CURRENT_OFFSET) << 8) + EEPROM.read(LOC_CURRENT_OFFSET+1);
 }
 
 void EEPROM_SetCurrentOffset(uint16_t currentOffset)
 {
-    EEPROM.write(4, currentOffset >> 8);
-    EEPROM.write(5, currentOffset & 0xff);  
+    EEPROM.write(LOC_CURRENT_OFFSET, currentOffset >> 8);
+    EEPROM.write(LOC_CURRENT_OFFSET+1, currentOffset & 0xff);  
 }
 
 uint16_t EEPROM_GetR1(void)
 {
-	return (EEPROM.read(6) << 8) + EEPROM.read(7);	
+	return (EEPROM.read(LOC_R1) << 8) + EEPROM.read(LOC_R1+1);	
 }
 
 void EEPROM_SetR1(uint16_t r1)
 {
-    EEPROM.write(6, r1 >> 8);
-    EEPROM.write(7, r1 & 0xff);  
+    EEPROM.write(LOC_R1, r1 >> 8);
+    EEPROM.write(LOC_R1+1, r1 & 0xff);  
 }
 
 uint16_t EEPROM_GetR2(void)
 {
-	return (EEPROM.read(8) << 8) + EEPROM.read(9);	
+	return (EEPROM.read(LOC_R2) << 8) + EEPROM.read(LOC_R2+1);	
 }
 
 void EEPROM_SetR2(uint16_t r2)
 {
-    EEPROM.write(8, r2 >> 8);
-    EEPROM.write(9, r2 & 0xff);  
+    EEPROM.write(LOC_R2, r2 >> 8);
+    EEPROM.write(LOC_R2+1, r2 & 0xff);  
 }
 
 uint16_t EEPROM_GetCurrentGain(void)
 {
-	return (EEPROM.read(10) << 8) + EEPROM.read(11);	
+	return (EEPROM.read(LOC_CURRENT_GAIN) << 8) + EEPROM.read(LOC_CURRENT_GAIN+1);	
 }
 
 void EEPROM_SetCurrentGain(uint16_t currentGain)
 {
-    EEPROM.write(10, currentGain >> 8);
-    EEPROM.write(11, currentGain & 0xff);  
+    EEPROM.write(LOC_CURRENT_GAIN, currentGain >> 8);
+    EEPROM.write(LOC_CURRENT_GAIN+1, currentGain & 0xff);  
 }
